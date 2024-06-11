7845e56f93ff0260c38d02b033a43c7857b2c163
Piod Gamer <malickelhadji07@gmail.com>
2024-06-11 10:55:33 +0000

    ca marche

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/userSchema");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 const router = express.Router();
 const auth = require("../middleware/verifyToken");
 const upload = require("../middleware/multer");
@@ -16,7 +17,10 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({ msg: "L'utilisateur existe déjà" });
     }
 
-    user = new User({ name, email, password });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user = new User({ name, email, password: hashedPassword });
     await user.save();
 
     const payload = { user: { id: user.id } };
@@ -78,7 +82,6 @@ router.get("/me", auth, async (req, res) => {
 });
 
 // Upload de l'image de profil
-// Logique pour mettre à jour l'image de profil d'un utilisateur
 router.post(
   "/updateProfileImage",
   auth,
@@ -108,22 +111,116 @@ router.post(
   }
 );
 
-// Par exemple, dans votre route qui gère l'affichage du profil d'un utilisateur
+// Afficher le profil de l'utilisateur
 router.get("/profile/:userId", async (req, res) => {
   try {
     const userId = req.params.userId;
 
-    // Récupérer l'URL de l'image de profil de l'utilisateur spécifié
     const userProfileImage = await User.findById(userId).select(
       "profileImageUrl"
     );
+
+    if (!userProfileImage) {
+      return res.status(404).json({ msg: "Utilisateur non trouvé" });
+    }
+
     const profileImageUrl = userProfileImage.profileImageUrl;
 
-    // Ensuite, vous pouvez envoyer cette URL dans la réponse pour l'affichage dans votre interface utilisateur
     res.json({ profileImageUrl });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Erreur du serveur");
+  }
+});
+
+function sendResetPasswordEmail(user, token) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "piodlords03@gmail.com",
+      pass: "cqce yoyv sbje mvim",
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: user.email,
+    subject: "Réinitialisation de mot de passe",
+    text: `Bonjour ${user.name},
+
+    Vous avez demandé une réinitialisation de mot de passe. Veuillez cliquer sur le lien ci-dessous pour réinitialiser votre mot de passe :
+    
+    http://localhost:3000/gmailPwd/${token}
+    
+    Si vous n'avez pas demandé cette réinitialisation, veuillez ignorer cet e-mail.
+
+    Cordialement,
+    RED PRODUCT`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email envoyé: " + info.response);
+    }
+  });
+}
+
+// Contrôleur pour la demande de réinitialisation de mot de passe
+router.post("/forgotPassword", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: "Utilisateur non trouvé !" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.RESET_PASSWORD_SECRET,
+      {
+        expiresIn: "1h", // Durée de validité du token de réinitialisation
+      }
+    );
+
+    sendResetPasswordEmail(user, token);
+
+    res.status(200).json({
+      message: "Un e-mail de réinitialisation de mot de passe a été envoyé.",
+    });
+  } catch (error) {
+    console.error("Error during forgot password:", error);
+    res.status(500).json({
+      error:
+        "Une erreur s'est produite lors de la demande de réinitialisation de mot de passe.",
+    });
+  }
+});
+
+// Contrôleur pour la soumission du formulaire de réinitialisation de mot de passe
+router.post("/resetPassword", async (req, res) => {
+  try {
+    const { token, password } = req.body;
+    const decoded = jwt.verify(token, process.env.RESET_PASSWORD_SECRET);
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "Utilisateur non trouvé !" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.status(200).json({ message: "Mot de passe réinitialisé avec succès." });
+  } catch (error) {
+    console.error("Error during reset password:", error);
+    res.status(500).json({
+      error:
+        "Une erreur s'est produite lors de la réinitialisation du mot de passe.",
+    });
   }
 });
 
